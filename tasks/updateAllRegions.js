e716b119d5b8187aa5799026605471b54d3a3063
Дмитрий Киселев <kiselev.dmitrii@mail.ru>
2020-07-01 13:49:58 +0300

    gitignore fix

--- tasks/updateAllRegions.js
+++ tasks/updateAllRegions.js
@@ -15,12 +15,16 @@ async function updateAllRegions() {
         });
 
         logger.info(response.data, `REDIS set ${regionId}`);
-        // пишем ответ api в кеш
+        // пишем ответ api в кеш (в данном случае я не указываю время жизни кеша, чтобы он никогда не инвалидировался автоматически)
         redis.set(regionId, JSON.stringify(response.data));
         // пишем regionId и unixTime в очередь для обновления
         const unixTime = Date.now();
         logger.info(`REDIS lpush ${regionId}:${unixTime}`);
         redis.lpush('updateList', `${regionId}:${unixTime}`);
+
+        // осталось запустить воркеры, которые будут выполнять BRPOP - блокирующая версия RPOP,
+        // которая гарантируюет получение сообщения только 1 воркером (независимо от их количества),
+        // или же можно использовать BRPOPLPUSH
 
         // чтобы не досить postman-echo.com, делаем запрос не чаще чем раз в секунду
         await waitFor(1000);
